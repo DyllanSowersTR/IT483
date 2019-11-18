102b39c2f321907c4a5958770ed83d2ac5d28260
Dyllan Sowers <dyllan.sowers@gmail.com>
2019-11-18 00:08:24 -0600

    F to C contents

---
+++ Fahrenheit to Celsius C#/FahrenheitToCelsius/FahrenheitToCelsius/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FahrenheitToCelsius
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while(true)
+            {
+                double celsius;
+                Console.WriteLine("Enter a temp in f: ");
+                double fahrenheit = Convert.ToDouble(Console.ReadLine());
+                celsius = (fahrenheit - 32) * 5 / 9;
+                Console.WriteLine("Celsius temp: " + celsius +"\nContinue? n to stop");
+                string cont = Convert.ToString(Console.ReadLine());
+                if(cont == "n")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
