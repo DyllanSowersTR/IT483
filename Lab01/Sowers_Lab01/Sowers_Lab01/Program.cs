b880d8344290f283bdede994b35991ab8972052b
dsowers <dyllan.sowers@gmail.com>
2019-11-19 17:03:58 -0600

    Created Lab01 files

---
+++ Lab01/Sowers_Lab01/Sowers_Lab01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Sowers_Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CS 201 Restaurant Guide\n");
+            string response;
+            char s, f;
+            bool spicy, fancy;
+
+            // Ask user for his/her preference
+            Console.WriteLine("Do you like spicy food? (y / n) : ");
+            // Get the next token
+            response = Convert.ToString(Console.ReadLine());
+            // Look only at the first character
+            s = response[0];
+            if (s == 'y' || s == 'Y')
+                spicy = true;
+            else
+                spicy = false;
+
+            // Ask user for his/her preference 
+            Console.WriteLine("Do you want to go to a fancy restaurant? (y / n) : ");
+            //Get the next token
+            response = Convert.ToString(Console.ReadLine());
+            f = response[0];
+            fancy = (f == 'y') || (f == 'Y');
+
+
+
+        }
+    }
+}
